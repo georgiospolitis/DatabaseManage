e05224ded715e9daa92fa824a0c058fe86221c5d
Georgios Politis <117930721+georgiospolitis@users.noreply.github.com>
2022-11-22 18:37:49 +0200

    Add files via upload

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab7C.Startup))]
+namespace Lab7C
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
